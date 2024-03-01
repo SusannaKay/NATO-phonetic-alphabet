54e3923bddb87a7030c268411457cbda505a874b
Susanna Kayed <kefka.sk@gmail.com>
2024-03-01 16:16:36 +0100

    added keyerror except + venv

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import pandas
 
 alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dic = {row.letter:row.code for (index, row) in alphabet.iterrows()}
-user_inp = input("Enter a word: ").upper()
-list = [nato_dic[letter] for letter in user_inp]
+
+while True:
+    user_inp = input("Enter a word: ").upper()
+    try: 
+        list = [nato_dic[letter] for letter in user_inp]
+        
+        print(list)
+        break
+    except KeyError:
+        print("sorry, only letters of the alphabet")
+    
+    
+    
 
 
-print(list)
 
